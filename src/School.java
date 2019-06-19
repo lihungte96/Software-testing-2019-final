98e34a329eaad38b4bc077f95df32b28b70e3ab7
robert <lihungte96@gmail.com>
2019-06-19 05:52:16 +0000

    final exam

---
+++ src/School.java
@@ -0,0 +1,6 @@
+public class School {
+	public String oralDefense(Student oralDefenser) {
+		String status = "Students: " + oralDefenser.getStuID() + "\n" + oralDefenser.getThesisName();
+		return status;
+	}
+}
---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public interface Student {
+
+    public String getStuID();
+
+    public String getThesisName();
+
+}
