cae134787f25d1a7f2c80b56a1b1a8eb023ef0e2
robert <lihungte96@gmail.com>
2019-06-19 09:34:44 +0000

    final exam clean

--- src/Distant.java
+++
@@ -1,9 +0,0 @@
-public class Distant {
-	public static int distant(int a, int b) {
-		if (a >= b) {
-			return a - b;
-		} else {
-			return b - a;
-		}
-	}
-}
--- src/School.java
+++ src/School.java
@@ -1,6 +1,7 @@
 public class School {
 	public String oralDefense(Student oralDefenser) {
-		String status = "Students: " + oralDefenser.getStuID() + "\n" + oralDefenser.getThesisName();
+		String status = "Students: " + oralDefenser.getStuID() + ", Thesis: " + oralDefenser.getThesisName();
+		System.out.println(status);
 		return status;
 	}
 }
